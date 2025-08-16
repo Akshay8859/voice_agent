50bd9a2d36c736049e28225b34aee824fbe0c776
Akshay <akshayakku7300@gmail.com>
2025-08-16 13:59:53 +0530

    added dashboard file

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Provider children={children}></Provider>
+        <Provider>
+          {children}
+        </Provider>
       </body>
     </html>
   );
